1b7b1caa6a437b51cff6b71bb8441650f9f5b6c2
Svet Pavlov <pavlov.svetoslav@gmail.com>
2023-03-13 12:27:48 -0700

    fix link anchors

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navigation = () => {
   return (
     <div className='sticky top-0 z-50 bg-dark flex gap-2 justify-evenly mt-2'>
-      <div className='nav-item w-32'>Svet Pavlov</div>
+      <a href="#pic"><div className='nav-item w-32'>Svet Pavlov</div></a>
       <div className='flex gap-2 justify-evenly'>
         <a href="#about"><div className='nav-item w-16'>About</div></a>
         <a href="#projects"><div className='nav-item w-20'>Projects</div></a>

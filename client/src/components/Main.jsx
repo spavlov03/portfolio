87564177414406d84127c4e2eaa1591201aafa1e
Svet Pavlov <pavlov.svetoslav@gmail.com>
2023-04-03 15:56:15 -0700

    email js bugs fixed, toast shows.

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import pic from '../assets/pic.jpeg'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer,toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import emailjs from 'emailjs-com'
 
@@ -38,6 +38,10 @@ const Main = () => {
     });
   };
 
+  const testToast = () => { 
+    toast("This is a test" , {position:"top-center"})
+  }
+
     const onSubmit = async (data) => {
       const { name, email,  message } = data;
       try {
@@ -67,7 +71,6 @@ const Main = () => {
         <div className='pt-10 pl-10'>
           <p className='text-green text-xl '>Hey there,</p>
           <p className='text-green text-5xl anchor' id='about'>I'm Svet!</p>
-          <button className='btn btn-blue' onClick={toastifySuccess()}>Toast</button>
         </div>
       </div>
       <p className='lg:w-2/3 w-5/6 mb-10'>About : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt saepe amet, est nulla temporibus eveniet accusamus perferendis expedita ut, et ullam debitis. Fugiat animi eius expedita officiis voluptate temporibus velit? Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cum velit omnis esse eaque odio cumque quisquam reprehenderit! Facilis, quas nam. Ducimus aliquam labore nulla molestiae delectus neque reprehenderit quaerat! Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae mollitia expedita fugit quibusdam minus, maiores itaque, tempora perspiciatis fugiat delectus voluptatum, deserunt placeat hic eligendi nemo cupiditate nihil non consequatur? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt saepe amet, est nulla temporibus eveniet accusamus perferendis expedita ut, et ullam debitis. Fugiat animi eius expedita officiis voluptate temporibus velit? Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cum velit omnis esse eaque odio cumque quisquam reprehenderit! Facilis, quas nam. Ducimus aliquam labore nulla molestiae delectus neque reprehenderit quaerat! Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae mollitia expedita fugit quibusdam minus, maiores itaque, tempora perspiciatis fugiat delectus voluptatum, deserunt placeat hic eligendi nemo cupiditate nihil non consequatur?</p>
@@ -130,7 +133,7 @@ const Main = () => {
         {errors.message && <span className='errorMessage'>Please enter a message</span>}
         <input type="submit" value="SEND" id='contactBtn' className='rounded'/>
       </form>
-
+      <ToastContainer/>
     </div>
   )
 }

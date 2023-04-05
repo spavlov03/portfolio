ae0d524ad0001df8c8d445bf18bc10c8945fbf1b
Svet Pavlov <pavlov.svetoslav@gmail.com>
2023-04-05 16:18:22 -0700

    another test

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,19 @@
 import './App.css';
 import Navigation from './components/Navigation';
 import Main from './components/Main';
-import Test from './components/Test';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+// import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
       <Navigation/>
-      <Routes>
+      <Main/>
+      {/* <Routes>
         <Route path='/' element={<Main/>}/>
-        <Route path='test' element={<Test/>}/>
-      </Routes>
-      </BrowserRouter>
+      </Routes> */}
+      {/* </BrowserRouter> */}
     </div>
   );
 }

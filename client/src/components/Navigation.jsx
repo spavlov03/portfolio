4f6aee3a41a8814ae4672f3ec3e3c0d1dcfa9841
Svet Pavlov <pavlov.svetoslav@gmail.com>
2023-04-26 16:26:19 -0700

    added toggle swtich

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -24,6 +24,11 @@ const Navigation = () => {
           </div> */}
 
         </div>
+        <label className="relative inline-flex items-center cursor-pointer">
+          <input type="checkbox" value="" className="sr-only peer"/>
+          <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-lime-500"></div>
+          <span class="ml-3 text-sm font-medium text-gray-300 dark:text-gray-300">Dark Mode</span>
+        </label>
         <div className='flex gap-2 justify-evenly'>
           <a className='flex items-center gap-2' href='https://github.com/spavlov03' target="_blank" rel="noreferrer">
             <img src={github} alt="GitGub" className='w-10 h-10 logo'/>
